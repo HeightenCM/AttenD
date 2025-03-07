22a3b0d2e58a0ceff2a0a7d073c9b76d3ff83d83
Heighten <hasconstantin@proton.me>
2025-03-07 20:41:37 +0200

    Work on CreateEventForm.kt

--- app/src/main/java/com/avilanii/attend/features/event/presentation/event_list/components/CreateEventDialog.kt
+++ app/src/main/java/com/avilanii/attend/features/event/presentation/event_list/components/CreateEventDialog.kt
@@ -1,9 +1,45 @@
 package com.avilanii.attend.features.event.presentation.event_list.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.avilanii.attend.ui.theme.AttenDTheme
 
 @Composable
 fun CreateEventDialog(modifier: Modifier = Modifier) {
-    
+    AlertDialog(
+        icon = {
+            Icon(Icons.Filled.Edit, "Create event icon")
+        },
+        title = {
+            Text("Create an event")
+        },
+        text = {
+
+        },
+        onDismissRequest = {
+
+        },
+        confirmButton = {
+
+        },
+        dismissButton = {
+
+        },
+
+    )
+}
+
+@PreviewLightDark
+@Composable
+private fun PreviewCreateEventDialog() {
+    AttenDTheme {
+        CreateEventDialog()
+    }
 }
